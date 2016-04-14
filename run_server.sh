9755a5ea2c4e5f42657ebda86ab981e204294c87
if1live <libsora25@gmail.com>
2016-04-14 10:49:54 +0900

    프로젝트 페이지에 다국어 지원 안붙어서 수정

--- run_server.sh
+++ run_server.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# example
+# ./run_server.sh en http://op-haruna.5ml.io:1377
+# ./run_server.sh ko http://op-haruna.5ml.io:1399
+
 function hugoserver_en {
 	hugo server --buildDrafts --verbose --config=config_en.toml --port=1377 --bind 0.0.0.0 --baseURL $1
 }
